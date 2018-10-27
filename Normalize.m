bfe907be3f47317caf8ea83fad8e0dfd418b9e6a
Magnus Strandberg <magnus.jo.strandberg@gmail.com>
2018-10-27 19:33:06 +0300

    Edited Data to not contain labels Added normalised test.csv

--- Normalize.m
+++ Normalize.m
@@ -1,4 +1,4 @@
-Data = [traindata trainlabels];
+Data = traindata;
 bands = 0:7;
 bands = bands*24;
 for i=1:7
@@ -24,3 +24,34 @@ for i=1:4
     max_B = max(Data{:,bands(i)+1:bands(i+1)},[],'all');
     Data{:,bands(i)+1:bands(i+1)} = Data{:,bands(i)+1:bands(i+1)}/max_B;
 end
+
+writetable(Data,'Data.csv')
+%test noramlization
+Test = testdata;
+bands = 0:7;
+bands = bands*24;
+for i=1:7
+    max_B = max(Test{:,bands(i)+1:bands(i+1)},[],'all');
+    Test{:,bands(i)+1:bands(i+1)} = Test{:,bands(i)+1:bands(i+1)}/max_B;
+end
+
+bands = 0:4;
+bands = bands.*12;
+bands = bands+168;
+
+for i=1:4
+    max_B = max(Test{:,bands(i)+1:bands(i+1)},[],'all');
+    Test{:,bands(i)+1:bands(i+1)} = Test{:,bands(i)+1:bands(i+1)}/max_B;
+end
+
+bands = 0:4;
+bands = bands*12;
+bands = bands+216;
+Data{:,217:220} = 0;
+
+for i=1:4
+    max_B = max(Test{:,bands(i)+1:bands(i+1)},[],'all');
+    Test{:,bands(i)+1:bands(i+1)} = Test{:,bands(i)+1:bands(i+1)}/max_B;
+end
+
+writetable(Test,'Test.csv')
