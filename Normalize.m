72a0fa4f4149a2e2d1554b2797d48d76b9c8c7ba
annahakala <anna.l.hakala@gmail.com>
2021-03-15 17:56:00 +0200

    Uploaded the project to github
    The Final results were never uploaded to GitHub as course recruitments demanded that it was uploaded on a different platform and Jupyter notebooks were not supported by GitHub back then.

--- Normalize.m
+++ Normalize.m
@@ -1,4 +1,7 @@
-Data = [traindata trainlabels];
+%The training and test data sets need to be imported into matlab first
+%With variable names testdata and traindata
+
+Data = traindata;
 bands = 0:7;
 bands = bands*24;
 for i=1:7
@@ -24,3 +27,34 @@ for i=1:4
     max_B = max(Data{:,bands(i)+1:bands(i+1)},[],'all');
     Data{:,bands(i)+1:bands(i+1)} = Data{:,bands(i)+1:bands(i+1)}/max_B;
 end
+Data.Variables =  round(Data.Variables,5);
+writetable(Data,'Data.csv')
+%test noramlization
+Test = testdata;
+bands = 0:7;
+bands = bands*24;
+for i=1:7
+    max_B = max(Test{:,bands(i)+1:bands(i+1)},[],'all');
+    Test{:,bands(i)+1:bands(i+1)} = Test{:,bands(i)+1:bands(i+1)}/max_B;
+end
+
+bands = 0:4;
+bands = bands.*12;
+bands = bands+168;
+
+for i=1:4
+    max_B = max(Test{:,bands(i)+1:bands(i+1)},[],'all');
+    Test{:,bands(i)+1:bands(i+1)} = Test{:,bands(i)+1:bands(i+1)}/max_B;
+end
+
+bands = 0:4;
+bands = bands*12;
+bands = bands+216;
+Test{:,217:220} = 0;
+
+for i=1:4
+    max_B = max(Test{:,bands(i)+1:bands(i+1)},[],'all');
+    Test{:,bands(i)+1:bands(i+1)} = Test{:,bands(i)+1:bands(i+1)}/max_B;
+end
+Test.Variables =  round(Test.Variables,5);
+writetable(Test,'Test.csv')
